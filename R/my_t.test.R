0e9142f445e40f8fd8c81d9003c5d0d4a1ea3b7a
Your Name <haoz515@uw.edu>
2020-12-08 22:50:19 -0800

    adding gap minder and my_t.test

---
+++ R/my_t.test.R
@@ -0,0 +1,47 @@
+#' a t.test function
+#'
+#' This fucntion do a t.test with three input parameters
+#'
+#' @param x a data vector with numeric values
+#' @param alternative a string to specify the test(greater, less or two.sided)
+#' @param mu a value of mean of null hypothesis
+#'
+#' @return a list, with mean of null hypothesis \code{mu}, and test type
+#' given by \code{alternative}
+#'
+#' @examples
+#'
+#' @export
+my_t.test <- function(x, alternative, mu) {
+  # do the test if the input is valid
+  if (alternative == "less" | alternative == "greater" |
+      alternative == "two.sided") {
+    length <- length(x)
+    # calculate the t.value
+    t_val <- (mean(x) - mu) / (sd(x) / sqrt(length))
+    # calculate the degree of freedom
+    df <- length - 1
+    # alternative hypothesis of less
+    if (alternative == "less") {
+      p_val = pt(-abs(t_val), df, lower.tail = FALSE)
+      return(list("test_stat is " = t_val, "degree of freedom is " = df,
+                  "alternative hypothesis: true mean is less than"
+                  = mu, "p-value is "= p_val))
+      # alternative hypothesis of greater
+    } else if (alternative == "greater") {
+      p_val = pt(-abs(t_val), df, lower.tail = TRUE)
+      return(list("test_stat is "= t_val, "degree of freedom is "= df,
+                  "alternative hypothesis: true mean is greater than"
+                  = mu, "p-value is "= p_val))
+    } else {
+      # alternative hypothesis of two.sided
+      p_val = 2 * pt(-abs(t_val), df, lower.tail = TRUE)
+      return(list("test_stat is "= t_val, "degree of freedom is "= df,
+                  "alternative hypothesis: true mean is not equal to"
+                  = mu, "p-value is "= p_val))
+    }
+    # throw the error message if the input is not valid
+  } else {
+    stop("The alternative can only be less, greater or two.sided")
+  }
+}
--- README.md
+++ README.md
@@ -2,3 +2,18 @@
   [![Travis build status](https://travis-ci.com/haoz515/HaoZheng.svg?branch=master)](https://travis-ci.com/haoz515/HaoZheng)
   [![Codecov test coverage](https://codecov.io/gh/haoz515/HaoZheng/branch/master/graph/badge.svg)](https://codecov.io/gh/haoz515/HaoZheng?branch=master)
   <!-- badges: end -->
+
+## Use
+
+The vignette demonstrates example usage of all main functions. Please [file an issue](https://github.com/haoz515n/corncob/issues) if you have a request for a tutorial that is not currently included. You can see the vignette by using the following code (note that this requires a TeX installation to view properly):
+
+
+``` r
+# install.packages("devtools")
+devtools::install_github("haoz515/corncob", build_vignette = TRUE, build_opts = c())
+library(corncob)
+# Use this to view the vignette in the corncob HTML help
+help(package = "corncob", help_type = "html")
+# Use this to view the vignette as an isolated HTML file
+utils::browseVignettes(package = "corncob")
+```
