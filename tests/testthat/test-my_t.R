ee21bfaa0c31ff36a07b597301607430c77308a4
Hao Zheng <649676388@qq.com>
2020-12-12 04:59:24 -0800

    update all the test except for my_knn

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -1,3 +1,8 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("the output is correct", {
+  expect_is(my_t.test(rnorm(10, 2, 1),"greater", 5), "list")
+})
+
+test_that("incorrect input throws error", {
+  expect_error(my_t.test("string"))
+  expect_error(my_t.test(2,3,4,4))
 })
