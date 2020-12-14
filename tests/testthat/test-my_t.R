3217b7541834ca00dd23cfeebe98944bd6895a9f
Hao Zheng <649676388@qq.com>
2020-12-13 22:27:36 -0800

    change my_t.test

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -1,5 +1,7 @@
 test_that("my_t.test works properly", {
   expect_is(my_t.test(rnorm(10, 0, 1),"less", 5), "list")
+  expect_is(my_t.test(rnorm(10, 0, 1),"greater", 5), "list")
+  expect_is(my_t.test(rnorm(10, 0, 1),"two.sided", 5), "list")
 })
 
 test_that("incorrect input throws error", {
