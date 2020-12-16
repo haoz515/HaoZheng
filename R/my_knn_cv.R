72caa387c3fd48bda9198b0ddaa36419eabcff8b
haoz515 <649676388@qq.com>
2020-12-16 05:10:04 -0800

    knn

--- R/my_knn_cv.R
+++ R/my_knn_cv.R
@@ -18,18 +18,17 @@
 #'
 #'
 #' @export
-
 my_knn_cv <- function(train, cl, k_nn, k_cv) {
   fold <- sample(rep(1:k_cv, length = len))
   cv_err <- rep(0, k_cv)
-  for (k in 1:k_cv) {
-    data_train_14 <- which(fold != k)
-    data_test_14 <- which(fold == k)
-    y_hat <- knn(train[data_train_14, ], train[data_test_14, ], cl[data_train_14], k_nn)
-    cv_err[k] <- (sum(y_hat != cl[data_test_14])) / length(data_test_14)
+  for (j in 1:k_cv) {
+    data_train_14 <- which(fold != j)
+    data_test_14 <- which(fold == j)
+    ppred <- class::knn(train[data_train_14, ], train[data_test_14, ], cl[data_train_14], k_nn)
+    cv_err[j] <- (sum(ppred != cl[data_test_14])) / length(data_test_14)
   }
   class <- class::knn(train, train, cl, k_nn)
-  return_cv_err <- mean(cv_err)
-  output <- list("class" = class, "cv_err" = return_cv_err)
-  return(output)
+  cv_err <- mean(cv_err)
+  lret <- list("class" = class, "cv_err" = cv_err)
+  return(lret)
 }
