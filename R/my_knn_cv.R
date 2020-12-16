58a7848db5355243ba200e6f722bd50e988a3c47
haoz515 <649676388@qq.com>
2020-12-16 05:02:07 -0800

    change knn

--- R/my_knn_cv.R
+++ R/my_knn_cv.R
@@ -20,18 +20,16 @@
 #' @export
 
 my_knn_cv <- function(train, cl, k_nn, k_cv) {
-  l <- length(cl)
   fold <- sample(rep(1:k_cv, length = len))
-  data <- data.frame("x" = train, "y" = cl, "split" = fold)
   cv_err <- rep(0, k_cv)
   for (i in 1:k_cv) {
     data_train_14 <- which(fold != i)
     data_test_14 <- which(fold == i)
-    y_hat <- knn(train[data_train_14, ], train[data_test_14, ], cl[data_train_14], k_nn)
-    # record the prediction and the misclassification rate
+    y_hat <- class::knn(train[data_train_14, ], train[data_test_14, ], cl[data_train_14], k_nn)
     cv_err[i] <- (sum(y_hat != cl[data_test_14])) / length(data_test_14)
   }
   class <- class::knn(train, train, cl, k_nn)
-  output <- list("class" = class, "cv_err" = mean(cv_err))
+  return_cv_err <- mean(cv_err)
+  output <- list("class" = class, "cv_err" = return_cv_err)
   return(output)
 }
